440c4f5069e9b0a767044e99dcd5bf70221a7b3d
guerrero-27 <markkevinguerrero7@gmail.com>
2025-09-17 21:45:27 +0800

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,6 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    // 👇 Ito ang kailangan para sa GitHub Pages
+    base: "/ElevateStudio/",
 });
